40992f3f2b2b2108b8ed4163b4850437aa57a4cf
anggiramadhan2910 <anggiramadhan2910@gmail.com>
2025-06-26 15:15:39 +0700

    Layouting System

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.rtl.min.css" integrity="sha384-Xbg45MqvDIk1e563NLpGEulpX6AvL404DP+/iCgW9eFa2BqztiwTexswJo2jLMue" crossorigin="anonymous">
+
+    <title>WPU Blog | {{ $title }}</title>
+  </head>
+  <body>
+
+  @include('partials.navbar');
+
+    <div class="container mt-4">
+        @yield('container')
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home') ;
+    return view('home',[
+        "title"=>"Home"
+    ]) ;
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title"=>"About",
         "name" => "Anggi Ramadhan",
         "email" => "AnggiRamadhan@gmail.com",
         "image" => "anggi.jpeg"
@@ -15,5 +18,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts') ;
+    return view('posts',[
+        "title"=>"Posts"
+    ]) ;
 });
