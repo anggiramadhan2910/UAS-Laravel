26697ff8dc4277a01d6fa3ec7a21925d6d3e45f6
anggiramadhan2910 <anggiramadhan2910@gmail.com>
2025-06-26 19:01:56 +0700

    Simulasi Blog Posts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('container')
+  <article>
+    <h2>Judul</h2>
+    <h5>Author</h5>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+        Facere officia dignissimos perferendis velit voluptate molestiae reiciendis dolorem a amet, 
+        suscipit architecto necessitatibus, 
+        distinctio maxime, incidunt ipsa consequuntur temporibus doloremque vero?</p>
+  </article>
+
+  <a href="/blog">Back To Posts</a>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,45 @@ Route::get('/about', function () {
     ]);
 });
 
+
 Route::get('/blog', function () {
+    $blog_posts = [
+    [
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Anggi Ramadhan",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae commodi fugiat accusantium dolor libero alias soluta omnis perferendis, 
+         aperiam dolorem nemo debitis nobis amet iusto animi dignissimos ducimus quasi illum pariatur mollitia saepe quos?
+         Officia distinctio temporibus tempore a eius, consectetur voluptas aut earum vero minima possimus optio nesciunt sit repudiandae mollitia soluta! 
+         Quas nesciunt, nobis eos, cum delectus aliquam harum obcaecati recusandae dolorum et ipsa soluta provident reprehenderit nihil!
+         Voluptatum necessitatibus pariatur asperiores accusamus id sapiente eligendi autem reprehenderit."
+    ],
+    [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Doddy Ferdiansyah",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque at consequuntur eveniet, necessitatibus molestiae nihil similique ut nemo perferendis qui? 
+        Ea eveniet facere exercitationem impedit unde quos corporis tempora blanditiis repellat reprehenderit quibusdam optio dolorum officia nesciunt atque ipsa in recusandae, 
+        ducimus eum nulla labore libero et magnam corrupti? Expedita sapiente quaerat libero. Animi enim praesentium beatae quo repellendus, 
+        libero blanditiis voluptas? Ea explicabo quaerat quis perferendis repellat, 
+        hic aut quia sapiente nisi. Impedit at animi fugit obcaecati ipsam, 
+        nostrum harum velit expedita molestias facere, ullam quae sed vitae quo doloremque corrupti reprehenderit praesentium facilis. 
+        Quod molestias aspernatur similique iusto."
+    ],
+];
+
+
     return view('posts',[
-        "title"=>"Posts"
+        "title"=>"Posts",
+        "posts" => $blog_posts
     ]) ;
 });
+
+
+//halaman single post
+Route::get('posts/{slug}', function($slug){
+    return view('post', [
+        "title" => "Single Post"
+    ]);
+});
+
